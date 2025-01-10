ed94d6d2191ab2e156217448ce2b384d84835f89
Amirmohamadsorani <amirmohamads1387@gmail.com>
2025-01-10 01:42:02 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>فروشگاه موکت</title>
+    <link rel="icon" href="images/logo/tools.png" type="image/ico"  />
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-rtl.min.css">
+    <link href="https://use.fontawesome.com/releases/v5.0.4/css/all.css" rel="stylesheet">
+    <link rel="stylesheet" href="moket.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+</head>
+<body class="">
+        <div id="home">
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+                <a href="#" class="navbar-brand">
+                </a>
+                <a class="navbar-brand" href="#">فروشگاه موکت امیر کارپت</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="moarefi_anvae_moket_porz_boland.php">معرفی انواع موکت پرز بلند<span class="sr-only"></span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link " href="moarefi_anvae_moket_porz_kotah.php"> مهرفی انواع موکت پرز کوتاه</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link " href="price_table.php">لیست قیمت</a>
+                    </li>
+                    </ul>
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                           <a class="nav-link " href="register.php">ثبت نام </a>
+                          </li>
+                          <li class="nav-item">
+                           <a class="nav-link " href="login.php">ورود</a>
+                          </li>
+                          <li class="nav-item">
+                           <a class="nav-link " href="sellform.php">فرم فروش</a>
+                          </li>
+                    </ul>
+                    
+                </div>
+            </nav><br>
+            <main class="p-3 bg-danger-subtle text-black">
+   <br>
+<br>
+<div>
+    <b>موکت کبریتی</b>
+</div><br>
+    <figure class="figure">
+    <img src="picture/keb.jpg" class="figure-img img-fluid rounded" class="rounded mx-auto d-block" alt="...">
+    </figure>
+    <p class="p">یکی دیگر از انواع موکت ها، موکت کبریتی است که احتمالاً آن را در بسیاری از فضاهای اداری و مسکونی دیده‌اید. این موکت‌ها معمولاً طراحی ساده ‌ای دارند. یکی از ساده ترین و ارزان ترین انواع موکت محسوب می‌شوند موکت‌های کبریتی معمولاً بافتی زبر و سختی دارند و از طرفی بسیار مقاوم هستند. قرار دادن اجسام سنگین روی نیمکت‌ها موجب آسیب دیدن به آن نمی‌شود و به دلیل ساده بودن می‌توانید از آن در هر فضایی که دوست دارید استفاده نمایید. معمولاً از موکت‌های کبریتی برای پوشاندن راهروی هتل‌ها و فضاهای پرتردد استفاده می‌شود، به همین دلیل به این نوع موکت‌ها موکت‌های هتلی نیز گفته می‌شود.</p>
+    <b>موکت نایلونی</b><br>
+    <br>
+    <figure class="figure">
+  <img src="picture/nailoni.jpg" class="figure-img img-fluid rounded" class="rounded mx-auto d-block" alt="...">
+</figure>
+    <p class="p">یکی از رایج ترین جنس‌هایی  که از آن برای تولید موکت استفاده می‌شود، نایلون است. موکت‌های نایلون بافتی نرم و یکپارچه دارند. از طرف دیگر در برابر لکه شدن و آسیب نیز بسیار مقاوم هستند. جالب است بدانید که تقریباً ۹۰ درصد موکت‌هایی که در خانه‌ها استفاده می‌شوند موکت‌های نایلونی یا پلی آمید هستند. این موکت‌ها در هتل‌ها نیز به وفور مورد استفاده قرار می‌گیرند، زیرا مقاومت بالایی در برابر پارگی و آسیب و کپک زدن دارند.</p>
+    <div>
+        <b>موکت الفین</b><br>
+    </div><br>
+    <figure class="figure">
+    <img src="picture/alfin.jpg"class="figure-img img-fluid rounded" class="rounded mx-auto d-block" alt="...">
+    </figure>
+    <p class="p">موکت الفین که به آن موکت پلی پروپلین نیز گفته می‌شود، یکی از محبوب ترین انواع موکت‌ها هستند که بیشتر برای محیط‌های اداری مورد استفاده قرار می‌گیرند. این موکت‌ها که به تازگی در فضاهای خانگی نیز استفاده می‌شوند، جنسی شبیه به پشم طبیعی دارند و از طرف دیگر مقاومت بالای این موکت موجب شده تا برای استفاده در فضاهای شلوغ و پرتردد بسیار مناسب باشند. مهم ‌ترین مزیت موکت‌های پلی پروپلین قیمت مناسب و نصب آسان آن است. از طرف دیگر این موکت‌ها تنوع بسیار زیادی دارند و تمیز کردن آن‌ها نیز بسیار ساده است.</p>
+    <div>
+    <b>موکت پلی استر</b><br>
+    </div><br>
+    <figure class="figure">
+    <img src="picture/poli ester.jpg" class="figure-img img-fluid rounded" class="rounded mx-auto d-block" alt="...">
+    </figure>
+    <p class="p">موکت پلی استر در واقع موکتی ضد آلرژی و ضد حساسیت است، با محیط زیست نیز سازگاری زیادی دارد و از مواد بازیافتی تهیه می‌شود. این نوع موکت‌ها مقاومت بالایی در برابر رنگ پریدگی دارند و تنوع رنگ و مدل‌های آن نیز بسیار زیاد است. مهم ترین موضوعی که باید در مورد موکت‌های پلی استر بدانید این است که قرار دادن اجسام سنگین روی موکت‌های پلی استر موجب آسیب به بافت موکت می‌شود. بنابراین این موکت معمولاً به ندرت برای فضاهای پر تردد استفاده می‌شود. به طور معمول قیمت موکت‌های پلی استر نسبت به‌ موکت‌های پشمی ‌و نایلونی ارزان تر است و از همین رو در دسته رایج ترین و محبوب ترین انواع موکت‌ها برای خانه محسوب می‌شوند. اگر از موکت‌های پلی استر به خوبی مراقبت شود تا ۱۵ سال بدون مشکل کار خواهند کرد.</p>
+    <div>
+        <b>انواع موکت پشمی</b>
+    </div>
+    <p class="p">یکی از گران ترین و لوکس ترین انواع موکت‌ها، موکت‌های پشمی‌ هستند. این موکت‌ها از پشم طبیعی تولید می‌شوند. به همین دلیل بسیار نرم هستند و ماندگاری بالایی دارند. معمولاً برای تولید این موکت‌ها از پشم ‌طبیعی، بدون هیچ ماده شیمیایی و مصنوعی استفاده می‌شود، به همین دلیل برای افراد حساس و افرادی که به آلرژی مبتلا هستند بسیار مناسب است. این موکت‌ها باید به درستی مراقبت شوند تا روی بافت آن کپک و سفیدک ایجاد نشود. به خاطر داشته باشید که استفاده از موکت‌های پشمی ‌برای مناطق شرجی و مرطوب به هیچ عنوان مناسب نیست.موکت‌های پشمی مقاومت بالایی دارند و تمیز کردن آن‌ها نیز بسیار ساده است این موکت‌ها با محیط زیست سازگار هستند و از طرف دیگر در برابر آتش نیز بسیار مقاوم هستند.</p>
+    <div>
+        <b>موکت تافتینگ</b><br>
+    </div><br>
+    <figure class="figure">
+    <img src="picture/tafting.jpg" class="figure-img img-fluid rounded" class="rounded mx-auto d-block" alt="...">
+    </figure>
+    <p class="p">موکت تافتینگ به موکت‌هایی شیک و چشمگیر گفته می‌شود که پرزهای بلندی دارند. به طور معمول برای تهیه این موکت‌ها از پشم طبیعی استفاده می‌شود. در بسیاری از مواقع نیز موکت‌های تافتینگ از ترکیب پشم و مواد مصنوعی مثل نایلون تولید می‌شوند. برای تهیه موکت‌های تافتینگ پرزهای بلند آن روی لایه زیرین بافته می‌شود و سپس به لایه دیگری چسبانده می‌شود. این موکت‌ها نیاز به مراقبت بیشتری نسبت به سایر انواع موکت‌ها دارند. زیرا در صورت مراقبت نکردن ممکن است پرز‌ها از روی موکت جدا شوند. بنابراین بهتر است از موکت‌های تافتینگ در مکان‌های پر تردد استفاده نشود.
+
+        تنوع بالا و رنگ بندی زیاد در کنار لطافت از مهم ترین ویژگی‌های موکت‌های تافتینگ هستند. این موکت‌ها شباهت زیادی به فرش دارند و از همین رو انتخاب مناسبی برای منزل و فضاهای مدرن هستند ضخامت بالای این موکت‌ها موجب گرم شدن فضای خانه می‌شود.  در رابطه با شستشوی موکت در منزل یشتر بدانید.</p>
+</main>
+<footer>
+    <div class="shadow p-3 mb-5 bg-body-tertiary rounded">
+   <div class="row justify-content-center">
+       <div class="col-md5 text-center">
+       
+           <p>
+             برای نصب و اجرای انواع موکت با شماره زیر تماس بگیر و از مشاوره رایگان ما بهره مند شوید
+           </p>
+           <p>
+                شماره تلفن : 09309161300
+            </p>
+               <a href="#">
+                   <i class="fab fa-twitter-square">
+
+                   </i>
+                   <i class="fab fa-instagram">
+
+                   </i>
+                   <i class="fab fa-facebook">
+
+                   </i>
+                   <i class="fab fa-youtube">
+
+                   </i>
+               </a>
+
+               <hr class="footer-hr">
+               <p>
+                   استفاده از مطالب سایت تنها با درج لینک مستقیم به آن مطلب مجاز است
+
+            </p>
+       </div>
+   </div>
+</div>
+</footer>
+</div>
+</body>
+</html>
